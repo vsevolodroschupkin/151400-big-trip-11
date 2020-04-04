41cc4a0a2f53ec2fc2a51da8fb45a450064b056c
User <user@MacBook-Pro-Vsevolod.local>
2020-04-04 21:56:35 +0500

    настроил сборку и разбил на компоненты

---
+++ src/components/tripDaysTemplate.js
@@ -0,0 +1,5 @@
+export const createTripDaysTemplate = () => {
+  return (
+    `<ul class="trip-days"></ul>`
+  );
+};
---
+++ src/components/tripInfoTemplate.js
@@ -0,0 +1,5 @@
+export const createTripInfoTemplate = () => {
+  return (
+    `<section class="trip-main__trip-info  trip-info"></section>`
+  );
+};
